453bcf69c789434ee2d77fc7998134994d94142a
ParkGwanHoo <hooho.dev@gmail.com>
2020-07-08 17:03:39 +0900

    reveal

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,14 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <V8Slide/>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import V8Slide from './components/v8-slide.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    V8Slide
   }
 }
 </script>
-
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/components/v8-slide.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="reveal">
+    <div class="slides">
+      <section class="present" style="top: 169px; display: block;">
+        <h1>V8</h1>
+        <h3>V8 javascript engine</h3>
+        <p>
+          <small>비즈서비스 개발팀  <a href="">박관후</a> / <a href="">ghpark@daou.co.kr</a></small>
+        </p>
+      </section>
+      <section>Slide1</section>
+      <section>Slide2</section>
+    </div>
+  </div>
+</template>
+<script>
+import Reveal from 'reveal.js/dist/reveal'
+export default {
+  name: 'V8Engine',
+  mounted() {
+    Reveal.initialize()
+  }
+}
+</script>
+<style scoped>
+  @import url('../../node_modules/reveal.js/dist/reveal.css');
+  @import url('../../node_modules/reveal.js/dist/theme/black.css');
+</style>
